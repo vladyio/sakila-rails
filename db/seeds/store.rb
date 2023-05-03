facdc994c1661ab009992d5841b477c5784d5c93
Vladislav Andreev <andreev@vlad.social>
2023-05-03 13:43:28 +0300

    Add stores seed

---
+++ db/seeds/store.rb
@@ -0,0 +1,11 @@
+store_copy_values = <<~SQL
+  ALTER TABLE stores DISABLE TRIGGER ALL;
+
+  INSERT INTO stores (id, manager_staff_id, address_id, created_at, updated_at) VALUES
+  (1, 1, 1, '2006-02-15 04:57:12', '2006-02-15 04:57:12'),
+  (2, 2, 2, '2006-02-15 04:57:12', '2006-02-15 04:57:12');
+
+  ALTER TABLE stores DISABLE TRIGGER ALL;
+SQL
+
+ActiveRecord::Base.connection.execute(store_copy_values)
