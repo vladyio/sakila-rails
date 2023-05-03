9e7d7fa7e23c6d9e165c5e6d564247070155e8b6
Vladislav Andreev <andreev@vlad.social>
2023-05-03 20:51:17 +0300

    Add cascading for Rental

---
+++ db/migrate/20230503175002_add_cascading_to_rental.rb
@@ -0,0 +1,15 @@
+class AddCascadingToRental < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key :rentals, :customers
+    add_foreign_key :rentals, :customers, null: false, index: true, on_delete: :restrict,
+                                          on_update: :cascade
+
+    remove_foreign_key :rentals, :inventories
+    add_foreign_key :rentals, :inventories, index: true, on_delete: :restrict,
+                                            on_update: :cascade
+
+    remove_foreign_key :rentals, :staff
+    add_foreign_key :rentals, :staff, null: false, index: true, on_delete: :restrict,
+                                      on_update: :cascade
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_174517) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_175002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -191,9 +191,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_174517) do
   add_foreign_key "payments", "customers", on_update: :cascade, on_delete: :restrict
   add_foreign_key "payments", "rentals", on_update: :cascade, on_delete: :restrict
   add_foreign_key "payments", "staff", on_update: :cascade, on_delete: :restrict
-  add_foreign_key "rentals", "customers"
-  add_foreign_key "rentals", "inventories"
-  add_foreign_key "rentals", "staff"
+  add_foreign_key "rentals", "customers", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "rentals", "inventories", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "rentals", "staff", on_update: :cascade, on_delete: :restrict
   add_foreign_key "staff", "addresses"
   add_foreign_key "staff", "stores"
   add_foreign_key "stores", "addresses"
