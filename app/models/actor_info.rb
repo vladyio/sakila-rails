f6667abc32c28eb66afc1923de1c28205779c946
Vladislav Andreev <andreev@vlad.social>
2023-05-04 13:01:27 +0300

    Generate annotation

--- app/models/actor_info.rb
+++ app/models/actor_info.rb
@@ -1,5 +1,13 @@
-# SQL View 'actor_info'
-
+# == Schema Information
+#
+# Table name: actor_info
+#
+#  id         :bigint
+#  film_info  :text
+#  first_name :string
+#  last_name  :string
+#
+# SQL View actor_info
 class ActorInfo < ApplicationRecord
   self.table_name = 'actor_info'
 end
