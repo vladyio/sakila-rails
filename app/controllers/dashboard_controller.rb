6d0146dd48009e5f7d8023124b72e3d81b9a2f3c
Vladislav Andreev <andreev@vlad.social>
2023-06-25 18:19:09 +0300

    Make dashboard item renderer polymorphic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,13 +3,14 @@
 class DashboardController < ApplicationController
   DASHBOARD_MODELS = %w[
     Store Staff Rental Payment Language Inventory Film
-    Customer Country City Category Address Actor].freeze
+    Customer Country City Category Address Actor
+  ].freeze
 
   layout -> { ApplicationLayout }
 
   def index
     @films = Film.all.sample(10)
     @dashboard_models = DASHBOARD_MODELS.sort
-    render Dashboard::IndexView.new(films: @films, models: @dashboard_models)
+    render Dashboard::IndexView.new(collection: @films, models: @dashboard_models)
   end
 end
--- app/views/dashboard/index_view.rb
+++ app/views/dashboard/index_view.rb
@@ -4,16 +4,20 @@ require File.join(Rails.root, 'app/views/components/dashboard/sidebar')
 require File.join(Rails.root, 'app/views/components/films/item')
 
 class Dashboard::IndexView < ApplicationView
-  def initialize(**collections)
-    @films = collections[:films]
-    @models = collections[:models]
+  def initialize(collection:, models:)
+    @collection = collection
+    @models = models
   end
 
   def template
     render Views::Components::Dashboard::Sidebar.new(@models)
 
     div(class: 'h-full ml-[235px] grid md:grid-cols-3 gap-1') do
-      @films.each { |film| render Views::Components::Film.new(film) }
+      @collection.each { |item| render dashboard_component(item.class, item) }
     end
+  end
+
+  def dashboard_component(model, item)
+    "Views::Components::#{model}".constantize.new(item)
   end
 end
