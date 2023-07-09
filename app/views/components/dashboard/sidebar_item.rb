0ac191adc4e26af293ceecf541b1cc3ba7bd1f69
Vladislav Andreev <andreev@vlad.social>
2023-07-09 21:58:00 +0300

    Add counters to sidebar

--- app/views/components/dashboard/sidebar_item.rb
+++ app/views/components/dashboard/sidebar_item.rb
@@ -10,7 +10,14 @@ module Views
       def template(&)
         li(class: 'relative mb-1') do
           a(class: 'navbar--item', href: helpers.root_path(model: @model),
-            data: { turbo_stream: true }) { @model.pluralize }
+            data: { turbo_stream: true }) do
+            span { @model.pluralize }
+
+            span(class: 'rounded-full bg-gray-700 ml-[0.3em] px-[0.65em]
+                          text-[0.75em] font-bold text-indigo-300') do
+              @model.constantize.count
+            end
+          end
         end
       end
     end
